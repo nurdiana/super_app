97bc5c24cc8b68371a6f6bbe3890b3d6c9dcf265
Nurdiana <nurdianasekolah@gmail.com>
2024-08-30 17:04:41 +0700

    Change main

--- lib/main.dart
+++ lib/main.dart
@@ -79,80 +79,89 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            myButton(
-              "Counter Page",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const CounterPage(
-                      title: "Counter Page",
-                    ),
-                  ),
-                );
-              },
+      body: Stack(
+        children: [
+          Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              //
+              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+              // action in the IDE, or press "p" in the console), to see the
+              // wireframe for each widget.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                myButton(
+                  "Counter Page",
+                  () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CounterPage(
+                          title: "Counter Page",
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                myButton(
+                  "Snake Page",
+                  () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SnakePage(
+                          title: "Snake Page",
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                myButton(
+                  "Compass",
+                  () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Compass(),
+                      ),
+                    );
+                  },
+                ),
+                myButton(
+                  "OpenStreetMap",
+                  () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const OpenStreetMapWidget(
+                          latitude: 48.8588443,
+                          longitude: 2.2943506,
+                          zoom: 13.0,
+                          useCurrentLocation: true,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-            myButton(
-              "Snake Page",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SnakePage(
-                      title: "Snake Page",
-                    ),
-                  ),
-                );
-              },
-            ),
-            myButton(
-              "Compass",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Compass(),
-                  ),
-                );
-              },
-            ),
-            myButton(
-              "OpenStreetMap",
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const OpenStreetMapWidget(
-                      latitude: 48.8588443,
-                      longitude: 2.2943506,
-                      zoom: 13.0,
-                      useCurrentLocation: true,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
+          ),
+          const Positioned(
+            right: 50,
+            bottom: 50,
+            child: Text("PlayGround app by Nurdiana"),
+          ),
+        ],
       ),
     );
   }
